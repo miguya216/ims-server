6d75cc90125ee878415794e12f29dcc35eff280d
miguya216 <johnmiguelrepuya216@gmail.com>
2025-10-20 11:17:24 +0800

    Update server.php

--- server.php
+++ server.php
@@ -5,18 +5,31 @@ use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 
-// Your Ratchet WebSocket handler
 class MyWebSocketHandler implements \Ratchet\MessageComponentInterface {
     public function onOpen(\Ratchet\ConnectionInterface $conn) {
         echo "New connection! ({$conn->resourceId})\n";
+
+        // Send welcome message as JSON
+        $conn->send(json_encode([
+            'type' => 'welcome',
+            'message' => "Welcome! Your connection ID is {$conn->resourceId}"
+        ]));
     }
+
     public function onMessage(\Ratchet\ConnectionInterface $from, $msg) {
         echo "Message received: $msg\n";
-        $from->send("Echo: $msg");
+
+        // Send back a JSON message
+        $from->send(json_encode([
+            'type' => 'echo',
+            'message' => $msg
+        ]));
     }
+
     public function onClose(\Ratchet\ConnectionInterface $conn) {
         echo "Connection {$conn->resourceId} has disconnected\n";
     }
+
     public function onError(\Ratchet\ConnectionInterface $conn, \Exception $e) {
         echo "Error: {$e->getMessage()}\n";
         $conn->close();
